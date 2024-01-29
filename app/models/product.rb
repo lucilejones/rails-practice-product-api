f3d757ee3b8c7e3e92d54a3d26d4953a2dec7726
Lucile Jones <jones.lucile@gmail.com>
2024-01-29 13:42:40 -0700

    practice creating relationships

---
+++ app/models/product.rb
@@ -0,0 +1,6 @@
+class Product < ApplicationRecord
+    validates :name, presence: true, uniqueness: true, length: { maximum: 50 }
+    validates :price, numericality: { greater_than: 0 }
+
+    belongs_to :category
+end
